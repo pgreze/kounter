7da41f37f4a0859ba3592be40bc67497a9464940
pgreze <pierrick.greze@gmail.com>
2020-05-02 01:55:42 +0900

    Add CI for build and publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    // Lint
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     id("io.gitlab.arturbosch.detekt") version "1.7.4"
+    // Deploy
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
 }
 
 group = "com.github.pgreze"
-version = properties.getOrDefault("VERSION", "WIP").toString()
+version = System.getenv("GITHUB_REF")?.trimStart('v') ?: "WIP"
+description = "Counting easily with Kotlin"
+val github = "https://github.com/pgreze/kounter"
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
@@ -32,4 +38,53 @@ dependencies {
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spek")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek")
     testImplementation(kotlin("reflect"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                url.set(github)
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("$github.git")
+                    developerConnection.set("scm:git:ssh://github.com:pgreze/kounter.git")
+                    url.set(github)
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+
+    setPublications("maven")
+
+    pkg.apply {
+        repo = "maven"
+        name = project.name
+        desc = project.description
+        websiteUrl = github
+        vcsUrl = "$github.git"
+        setLicenses("Apache-2.0")
+        publish = true
+        publicDownloadNumbers = true
+        version.apply {
+            desc = project.description
+            gpg.apply {
+                // Determines whether to GPG sign the files (default: false)
+                sign = true
+                // The passphrase for GPG signing (optional)
+                passphrase = System.getenv("BINTRAY_GPG_PASSWORD")
+            }
+        }
+    }
 }
